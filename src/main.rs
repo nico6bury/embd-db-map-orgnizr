940a1cbd86d0eeba9c6b53a350b2fae154560870
Nicholas Sixbury <nico6bury@ksu.edu>
2023-11-29 02:54:31 -0600

    Fleshed out main method for testing GUI functions.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,37 @@ fn main() {
     let mut gui = GUI::default();
     gui.initialize();
 
+    let mut map_list: Vec<String> = Vec::new();
+    map_list.push("map1".to_string());
+    map_list.push("map2".to_string());
+    map_list.push("obvious placeholder is obvious".to_string());
+    gui.update_map_view_list(&map_list);
+
     gui.show();
     while gui.app.wait() {
-        // do whatever
-    }
+        if let Some(val) = gui.msg_receiver.recv() {
+            // do something
+            println!("{}", val);
+            if val.eq("select-new-map-file") {
+                println!("We should open file dialog!");
+                match GUI::dialog_get_file() {
+                    Some(val) => {
+                        // placeholder for debugging
+                        println!("{}", val);
+                    },
+                    None => println!("file dialog cancelled or path empty"),
+                }//end making sure we got something from dialog
+            }//end if user wants to select a file
+            else if val.eq("select-new-map-folder") {
+                println!("We should open folder dialog!");
+                match GUI::dialog_get_folder() {
+                    Some(val) => {
+                        // placeholder for debugging
+                        println!("{}", val);
+                    },
+                    None => println!("folder dialog cancelled or path empty"),
+                }
+            }//end if user wants to select a folder
+        }//end if we got a message from gui
+    }//end main app loop
 }//end main fn
